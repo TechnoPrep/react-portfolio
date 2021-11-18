074d7cfd939c4b3b9c2b16af74189ed8be3b7622
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-18 14:00:14 -0700

    Updated css style names for css consolidation

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import remindrLogo from "../images/remindr_logo.png";
-import ecommerce from "../images/E-Commerce.png";
-import cairn from "../images/Cairn.png";
+import techblog from "../images/Tech_Blog.png";
+import cairn from "../images/Cairn_v2.png";
 
 const Projects = () => {
   return (
     <Router>
-      <main className="py-5">
+      <main className="py-5 secondary-bg-custom">
         <div className="row col-12 d-flex align-items-top">
           <div className="container">
             <Cards />
@@ -21,79 +21,108 @@ const Projects = () => {
 
 const Cards = () => {
   return (
-    <div class="container">
-      <div
-        className="row secondary-bg-custom d-flex align-content-center"
-        style={{ position: "relative", height: "690px" }}
-      >
-        <div className="col-sm-6 col-lg-4">
-          <div className="card">
-            <div className="project-img">
-              <img
-                className="card-img-top"
-                src={remindrLogo}
-                alt=""
-                srcset=""
-              ></img>
-            </div>
+    <div class="d-flex justify-content-center">
+      <div class="w-85-custom">
+        <div className="d-flex justify-content-center">
+            <h5 className="roboto-400 light-teal-text text-start section-header-text">
+              Projects
+            </h5>
+        </div>
+        <div
+          className="row d-flex align-content-center"
+          style={{ position: "relative", height: "504px" }}
+        >
 
-            <div className="card-body">
-              <h5 className="card-title">
-                Card title that wraps to a new line
-              </h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+          <div className="col-sm-6 col-lg-4 d-flex justify-content-center">
+            <div className="card main-bg-custom card-custom w-85-custom light-teal-shadow-border">
+              <div className="project-img p-3 main-bg-custom">
+                <img
+                  className="card-img-top "
+                  src={remindrLogo}
+                  alt=""
+                ></img>
+              </div>
+  
+              <div className="card-body card-body-custom main-bg-custom">
+                <h5 className="card-title text-white">
+                  Remindr: Notification App
+                </h5>
+                <p className="card-text text-white">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <div className="d-flex justify-content-end">
+                  <button className="text-white view-project-btn"
+                  onClick={() => {
+                    window.location.href='https://github.com/LoopySquare/Remindr';
+                    }}>
+                    View Project
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-sm-6 col-lg-4">
-          <div className="card">
-            <div className="project-img">
-              <img
-                className="card-img-top"
-                src={cairn}
-                alt=""
-                srcset=""
-              ></img>
-            </div>
-
-            <div className="card-body">
-              <h5 className="card-title">
-                Card title that wraps to a new line
-              </h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+  
+          <div className="col-sm-6 col-lg-4 d-flex justify-content-center">
+            <div className="card main-bg-custom card-custom w-85-custom light-teal-shadow-border">
+              <div className="project-img p-3 main-bg-custom">
+                <img
+                  className="card-img-top"
+                  src={cairn}
+                  alt=""
+                ></img>
+              </div>
+  
+              <div className="card-body card-body-custom main-bg-custom">
+                <h5 className="card-title text-white">
+                  Cairn: National Park API
+                </h5>
+                <p className="card-text text-white">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <div className="d-flex justify-content-end">
+                  <button className="text-white view-project-btn"
+                  onClick={() => {
+                    window.location.href='https://github.com/TechnoPrep/cairn';
+                    }}>
+                    View Project
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-sm-6 col-lg-4">
-          <div className="card">
-            <div className="project-img">
-              <img
-                className="card-img-top"
-                src={ecommerce}
-                alt=""
-                srcset=""
-              ></img>
-            </div>
-
-            <div className="card-body">
-              <h5 className="card-title">
-                Card title that wraps to a new line
-              </h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+  
+          <div className="col-sm-6 col-lg-4 d-flex justify-content-center">
+            <div className="card main-bg-custom card-custom w-85-custom light-teal-shadow-border">
+              <div className="project-img p-3 main-bg-custom">
+                <img
+                  className="card-img-top"
+                  src={techblog}
+                  alt=""
+                ></img>
+              </div>
+  
+              <div className="card-body card-body-custom main-bg-custom">
+                <h5 className="card-title text-white">
+                  Tech Blog
+                </h5>
+                <p className="card-text text-white">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <div className="d-flex justify-content-end">
+                  <button className="text-white view-project-btn"
+                  onClick={() => {
+                    window.location.href='https://github.com/TechnoPrep/tech-blog';
+                    }}>
+                    View Project
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>

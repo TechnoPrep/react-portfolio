6272b0dd307efe611f9140bd94c9326e0059b4f7
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-18 13:56:42 -0700

    Updated styling for hover affect for links in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -8,18 +8,20 @@ const Header = () => {
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <div className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <Link className="text-light" to="/">
-              <h1 className="m-0 roboto-400">NE<span className="home-span">.</span></h1>
+              <h1 className="m-0 roboto-400 ">
+                NE<span className="home-span">.</span>
+              </h1>
             </Link>
           </div>
           <div className="text-end">
             <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li>
-                <Link className="nav-link px-4 text-white" to="/projects">
-                  <h2 className="m-0 roboto-400">Projects</h2>
+                <Link className="nav-link px-4 text-white " to="/projects">
+                  <h2 className="m-0 roboto-400 ">Projects</h2>
                 </Link>
               </li>
               <li>
-                <Link className="nav-link px-4 text-white" to="/contact">
+                <Link className="nav-link px-4 text-white " to="/contact">
                   <h2 className="m-0 roboto-400">Contact</h2>
                 </Link>
               </li>

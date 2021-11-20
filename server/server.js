44133f6bd7c6303e4d7dc3afe137b08421c56098
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2021-11-20 15:42:23 -0700

    Changed file structure to allow server.js integration, https forwarding

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+app.use(function(request, response){
+  if(!request.secure){
+    response.redirect("https://" + request.headers.host + request.url);
+  }
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
+

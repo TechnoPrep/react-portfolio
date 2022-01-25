d416cdef111c82efcf60f6939ecd89b8da075d7c
Nathaniel Ehrlich <nathan.ehrlich2621@gmail.com>
2022-01-24 23:50:54 -0700

    Added useState and useEffect for mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -11,21 +11,43 @@ import Footer from './components/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
-  return (
+  
+  const [width, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    updateDimensions()
+
+    window.addEventListener("resize", updateDimensions);
+
+    console.log('I Ran');
+
+    return () => {
+      window.removeEventListener("resize", updateDimensions);
+    }
+  }, [])
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setWindowWidth(width);
+    console.log('updateDimensions', width);
+  }
+
+  const responsive =  width > 1115;
+  
+  return (    
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="main-bg-custom">
             <Route exact path="/">
-              <Home />
+              <Home useW50 = {responsive} />
             </Route>
             <Route exact path="/projects">
-              <Projects />
+              <Projects useW50 = {responsive} />
             </Route>
             <Route exact path="/contact">
-              <Contact />
+              <Contact useW50 = {responsive} />
             </Route>
           </div>
           <Footer />
